afdef1d824cc43ff3b9566ee28e7b9394a152d59
simon <simon.lu@tron.network>
2022-12-14 10:07:39 +0800

    test

--- test/SicoToken.js
+++ test/SicoToken.js
@@ -12,20 +12,21 @@ describe("SicoToken contract", function () {
   let tokenBlockReward = 50;
 
   beforeEach(async function () {
+    // Get the ContractFactory and Signers here.
     Token = await ethers.getContractFactory("SicoToken");
-    [owner, addr1, addr2] = await hre.ethers.getSigner();
+    [owner, addr1, addr2] = await hre.ethers.getSigners();
 
     sicoToken = await Token.deploy(tokenCap, tokenBlockReward);
   });
 
   describe("Deployment", function () {
     it("Should set the right owner", async function () {
-      expect(await sicoToken.owner().to.equal(owner.address));
+      expect(await sicoToken.owner()).to.equal(owner.address);
     });
 
     it("Should assign the total supply of tokens to the owner", async function () {
       const ownerBalance = await sicoToken.balanceOf(owner.address);
-      expect(await sicoToken.totalSupply().to.equal(ownerBalance));
+      expect(await sicoToken.totalSupply()).to.equal(ownerBalance);
     });
 
     it("Should set the max capped supply to the argument provided during deployment", async function () {
@@ -41,13 +42,15 @@ describe("SicoToken contract", function () {
     });
   });
 
-  describe("Transaction", function () {
+  describe("Transactions", function () {
     it("Should transfer tokens between accounts", async function () {
       // Transfer 50 tokens from owner to addr1
       await sicoToken.transfer(addr1.address, 50);
       const addr1Balance = await sicoToken.balanceOf(addr1.address);
       expect(addr1Balance).to.equal(50);
 
+      // Transfer 50 tokens from addr1 to addr2
+      // We use .connect(signer) to send a transaction from another account
       await sicoToken.connect(addr1).transfer(addr2.address, 50);
       const addr2Balance = await sicoToken.balanceOf(addr2.address);
       expect(addr2Balance).to.equal(50);
@@ -55,21 +58,28 @@ describe("SicoToken contract", function () {
 
     it("Should fail if sender doesn't have enough tokens", async function () {
       const initialOwnerBalance = await sicoToken.balanceOf(owner.address);
+      // Try to send 1 token from addr1 (0 tokens) to owner (1000000 tokens).
+      // `require` will evaluate false and revert the transaction.
       await expect(
         sicoToken.connect(addr1).transfer(owner.address, 1)
       ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
 
+      // Owner balance shouldn't have changed.
       expect(await sicoToken.balanceOf(owner.address)).to.equal(
         initialOwnerBalance
       );
     });
 
-    it("Should update balances after transfer", async function () {
+    it("Should update balances after transfers", async function () {
       const initialOwnerBalance = await sicoToken.balanceOf(owner.address);
 
+      // Transfer 100 tokens from owner to addr1.
       await sicoToken.transfer(addr1.address, 100);
+
+      // Transfer another 50 tokens from owner to addr2.
       await sicoToken.transfer(addr2.address, 50);
 
+      // Check balances.
       const finalOwnerBalance = await sicoToken.balanceOf(owner.address);
       expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(150));
 
